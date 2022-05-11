fb68548ad51790c5e03aeecbb4a257798eba489f
Nastya <21nastya.chernaya19@gmail.com>
2022-05-11 19:20:28 +0300

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateValue = new DateTime(2008, 6, 11);
+            Console.WriteLine(dateValue.ToString("dd число" + " ( dddd )"));
+
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
